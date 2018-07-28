13ecae88ee07d7f6f0cca7753d915cbb1150d012
chetan shinde <chetan.shinde@exadatum.com>
2018-07-28 13:23:26 +0530

    The test case needed changes.

--- CleanFolders/sample.py
+++ CleanFolders/sample.py
@@ -1,31 +1,34 @@
 from CleanFolders import ManageImages as mi
 import os
 import datetime
+import unittest
 
 
-def test_copy_action():
-    source_path = '/Users/chetanshinde/Documents'
-    destination_path = '/Users/chetanshinde/Documents/Photos/Test'
-    folder_date = datetime.datetime.today().strftime('%Y-%m-%d')
-    destination_path = destination_path + "/" + folder_date
+class TestingImageManagement(unittest.TestCase):
 
-    process = mi.ManageImages(source_path, destination_path)
+    def test_copy_action(self):
+        source_path = '/Users/chetanshinde/Documents'
+        destination_path = '/Users/chetanshinde/Documents/Photos/Test'
+        folder_date = datetime.datetime.today().strftime('%Y-%m-%d')
+        destination_path = destination_path + "/" + folder_date
 
-    for name, dirName in process.get_image_files():
-        print(dirName)
-        process.move_to_target(dirName + "/" + name)
+        process = mi.ManageImages(source_path, destination_path)
 
-    print(destination_path)
+        for name, dirName in process.get_image_files():
+            # print(dirName)
+            process.move_to_target(dirName + "/" + name)
 
-    no_of_copied = 0
-    try:
-        no_of_copied = len([f for f in os.listdir(destination_path) if os.path.isfile(os.path.join(destination_path, f))])
-    except Exception as e:
-        pass
+        # print(destination_path)
 
-    print("The number of copied images are ---- ",no_of_copied)
-    assert no_of_copied == 3
+        no_of_copied = 0
+        try:
+            no_of_copied = len([f for f in os.listdir(destination_path) if os.path.isfile(os.path.join(destination_path, f))])
+        except Exception as e:
+            pass
+
+        # print("The number of copied images are ---- ",no_of_copied)
+        self.assertEqual(no_of_copied, 2, msg="The number of images copied is not as Expected")
 
 
 if __name__ == '__main__':
-    test_copy_action()
+    unittest.test_copy_action()
